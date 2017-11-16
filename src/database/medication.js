ac3dada3e8ad652e5f076295403f5a69d60c861b
BenTurcotte <bturcott@mail.uoguelph.ca>
2017-11-16 14:15:53 -0500

    transferred some details about the medications table from the google doc

--- src/database/medication.js
+++ src/database/medication.js
@@ -6,6 +6,23 @@ module.exports = function(db, userDB) {
     return {
         /**
          * This will create the medication table
+         * 
+         * Columns (as specified in the google doc):
+         * - user_id
+         * - name
+         * - dosage_amount
+         * - dosage_unit
+         * - start_date
+         * - end_date
+         * - frequency
+         * - days_of_week : each day stores a string with comma-separated times
+         * - notes
+         * - notification
+         * - notification_before
+         * - hits
+         * - misses
+         * 
+         * link: https://docs.google.com/document/d/19EfGXJbhmD3z-u2Ud4jsdQaSt6Fuxf-8L5xDz8mQ5cc/edit?usp=sharing
          */
         createTable() {
 
