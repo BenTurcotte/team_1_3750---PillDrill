8864ae0809c182982e7e6c974b3464097358f3f6
BenTurcotte <bturcott@mail.uoguelph.ca>
2017-11-16 14:08:29 -0500

    started on the epidermis of the medication-related endpoints.

--- src/api/medication.js
+++ src/api/medication.js
@@ -6,8 +6,19 @@ const utils = require("../utils");
 
 module.exports = function(route) {
 
+    /*
+    req has auth token and UID
+    */
+    router.post("/GetMedications", function(req, res) {
+        
+    });
+
+    /*
+    req has auth token and UID
+    */
     router.post("/GetSchedule", function(req, res) {
-        // check authentication token
+        // authTok = req.body.authenticationToken;
+        // check authentication token with db to grant access
         // retrieve all medication for the given userID from the db
         /*
           for every medication with more than one specified time
@@ -20,9 +31,25 @@ module.exports = function(route) {
           for each day in the array
             for each medication in the list of medications
               if the medication is to be taken on the day in question,
+              AND the day in question is not past the end date or before the
+              start date
                 add it to the list at the day specified by the array index
         */
         // res = giveMeJSON(dayArray)
     });
 
+    /*
+    req has auth token, UID, and medID
+    if medID is not provided, a new medication should be added
+    */
+    router.post("/UpdateMedication", function(req, res) {
+
+    });
+    
+    /*
+    req has auth token, UID, and medID
+    */
+    router.post("/DeleteMedication", function(req, res) {
+
+    });
 };
