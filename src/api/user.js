59f8a0b1cde851fc0c820472476cd041e8fa0be3
BenTurcotte <bturcott@mail.uoguelph.ca>
2017-11-18 14:50:13 -0500

    started on createTable function for medications (database/). Also worked on the create new user function (api/)

--- src/api/user.js
+++ src/api/user.js
@@ -3,10 +3,12 @@ This is the base route for all of our user routes
 This is the only file that main.js should be interacting with in the user/ directory
 */
 
-const express = require("express");
+const express    = require("express");
 
 //Include all of our submodules that we will pass our router
-const login = require("./login");
+const utils      = require('../utils');
+const dbUser     = require('../database/user');
+const login      = require("./login");
 const medication = require("./medication");
 
 //Create a router to create different paths related to the users
@@ -16,4 +18,17 @@ var router = express.Router();
 login(router);
 medication(router);
 
-module.exports = router;
+module.exports = function (router) {
+    
+    req.on('error', (err) => { utils.createErrorObject(err) }); // ??? what is err? hopefully a string...
+    
+    /*
+    request has auth_token, userID, client_JSON
+    */
+    router.post("/CreateAccount", function (req, res) {
+        var { email, password, accountType } = JSON.parse(req.body);
+        dbUser.createNewUser(email, password, accountType, function () {
+            // callback func goes here
+        });
+    });
+};
--- src/database/medication.js
+++ src/database/medication.js
@@ -2,6 +2,8 @@
 This file will hold all of the function calls for the medications
 */
 
+const MEDICATION_TABLE_NAME = "Medications";
+
 module.exports = function(db, userDB) {
     return {
         /**
@@ -24,8 +26,26 @@ module.exports = function(db, userDB) {
          * 
          * link: https://docs.google.com/document/d/19EfGXJbhmD3z-u2Ud4jsdQaSt6Fuxf-8L5xDz8mQ5cc/edit?usp=sharing
          */
+        /**
+         * Creates the user table and will not drop it if it's already there
+         */
         createTable() {
-
+            db.run(`CREATE TABLE IF NOT EXISTS ${MEDICATION_TABLE_NAME} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id TEXT UNIQUE,
+                name TEXT NOT NULL,
+                dosage INTEGER,
+                dosage_unit TEXT NOT NULL,
+                start_date TEXT NOT NULL,
+                end_date TEXT NOT NULL,
+                frequency INTEGER,
+                days_of_week TEXT NOT NULL,
+                notes TEXT,
+                notification INTEGER,
+                notification_before INTEGER,
+                hits INTEGER,
+                misses INTEGER
+            )`);
         }
     };
 };
