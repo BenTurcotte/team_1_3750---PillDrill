2c81a39d4127b682dc4b4fad1b504ec4ebdcfb65
bencecile <bencecile@gmail.com>
2017-11-20 18:01:10 -0500

    Fixed the 'route' parameter name to 'router'

--- src/api/medication.js
+++ src/api/medication.js
@@ -4,7 +4,7 @@ This will be the routes for the medications
 const db    = require("../database/database");
 const utils = require("../utils");
 
-module.exports = function(route) {
+module.exports = function(router) {
 
     /*
     req has auth token and UID
