5dcb4a255676138de984481b80dce1971e6b5053
CIS3750 Production Server <bcecile@uoguelph.ca>
2017-11-20 18:40:24 -0500

    Added the required CORS header

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ app.disable("etag");
 
 app.use(function(req, res, next) {
     res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
     next();
 });
 
