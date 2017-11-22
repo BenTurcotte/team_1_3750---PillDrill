86cf9016c6e890835e5baa5088c9d8c17bd8750b
tcharcho <tcharcho@uoguelph.ca>
2017-11-21 21:43:34 -0500

    checkLogin login_token_expires validation

--- src/database/user.js
+++ src/database/user.js
@@ -220,7 +220,8 @@ module.exports = function(db) {
 
                 //Check that login_tokenDate has not passed
                 currDate = new Date()
-                if (currDate > row.login_token_expires){
+                exp_date = new Date(row.login_token_expires)
+                if (currDate > exp_date){
                     callback(new Error("Login session has expired"));
                     return;
                 }       
